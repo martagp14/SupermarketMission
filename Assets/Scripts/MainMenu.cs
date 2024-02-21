19ed0b90e0508ef8f362bce75f09541f8e27c554
martagp14 <57727166+martagp14@users.noreply.github.com>
2024-02-21 18:03:00 +0100

    Menu Scene Added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private object playButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene("StartingCinematic");
+    }
+
+    public void OnClickScoreboard()
+    {
+        Debug.Log("Clicked Scoreboard");
+    }
+
+    public void OnClickOptions()
+    {
+        Debug.Log("Clicked Options");
+    }
+
+    public void OnClickExit()
+    {
+        Debug.Log("Clicked Exit");
+        Application.Quit();
+    }
+}
