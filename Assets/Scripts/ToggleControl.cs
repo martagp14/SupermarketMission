a452b4efd5658576d7854210ac597c6420f61d22
martagp14 <57727166+martagp14@users.noreply.github.com>
2024-03-20 11:08:45 +0100

    Supermarket map with button and item selection section

---
+++ Assets/Scripts/ToggleControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleControl : MonoBehaviour
+{
+    private Toggle[] toggles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggles = GetComponentsInChildren<Toggle>();
+    }
+
+    public void selectedObjects()
+    {
+        foreach (Toggle t in toggles)
+        {
+            Debug.Log(t.isOn);
+        }
+    }
+}
