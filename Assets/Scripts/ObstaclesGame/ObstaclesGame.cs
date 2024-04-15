3864ea673536e78e793ac963b073ff9d2b3dd70a
martagp14 <57727166+martagp14@users.noreply.github.com>
2024-04-15 11:58:06 +0200

    Lot of things and Obstacles Game

--- Assets/Scripts/ObstaclesGame/ObstaclesGame.cs
+++ Assets/Scripts/ObstaclesGame/ObstaclesGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObstaclesGame : MonoBehaviour
 {
@@ -8,10 +9,18 @@ public class ObstaclesGame : MonoBehaviour
 
     private Vector3[] spawnPoints = { new Vector3(-4.5f, 3.9000001f, 58.4000015f), new Vector3(0f, 3.9000001f, 58.4000015f), new Vector3(4.5f, 3.9000001f, 58.4000015f) };
 
+    private int playerLifes;
+    private bool isGameOver;
+    private int numObstacles;
+
+    [SerializeField] private List<Image> hearts = new List<Image>(3);
 
     // Start is called before the first frame update
     void Start()
     {
+        numObstacles = 0;
+        isGameOver = false;
+        playerLifes = hearts.Count;
         StartCoroutine(SpawnObjects());
     }
 
@@ -23,12 +32,34 @@ public class ObstaclesGame : MonoBehaviour
 
     IEnumerator SpawnObjects()
     {
-        for(int i=0; i<5; i++)
+        while(!isGameOver)
         {
             var index = Random.Range(0, 3);
             yield return new WaitForSeconds(1f);
             var obs = Instantiate(obstacle);
             obs.gameObject.transform.SetPositionAndRotation(spawnPoints[index], new Quaternion(-0.707106829f, 0, 0, 0.707106829f));
         }
+    }
+
+    public void DamagePlayer()
+    {
+        Debug.Log("AUCH");
+        hearts[playerLifes].gameObject.SetActive(false);
+        playerLifes--;
+        if (playerLifes == 0)
+        {
+            this.GameOver();
+        }
+    }
+
+    public void ObstacleReachedTheEnd()
+    {
+        this.numObstacles++;
+    }
+
+    private void GameOver()
+    {
+        //Pausar carrito y obstaculos
+        //Mostrar canvas de GameOver para pasar a la siguiente escena
     }
 }
--- Assets/Scripts/ObstaclesGame/TrolleyMovement.cs
+++ Assets/Scripts/ObstaclesGame/TrolleyMovement.cs
@@ -6,17 +6,40 @@ public class TrolleyMovement : MonoBehaviour
 {
 
     private ObstaclesGame miniGameManager;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         miniGameManager = FindObjectOfType<ObstaclesGame>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A)|| Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetTrigger("MoveLeft");
+            StartCoroutine(DeactivateTrigger());
+        }
+        if (Input.GetKeyDown(KeyCode.D)|| Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetTrigger("MoveRight");
+            StartCoroutine(DeactivateTrigger());
+        }
     }
 
     //Movement of the trolley using arrow or mouse
+    IEnumerator DeactivateTrigger()
+    {
+        yield return new WaitForSeconds(0.5f);
+        anim.ResetTrigger("MoveLeft");
+        anim.ResetTrigger("MoveRight");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("Damaged");
+        miniGameManager.DamagePlayer();
+    }
 }
